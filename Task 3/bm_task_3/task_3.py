aba13a29b160785e3a1f90147596333e11ba9b63
madpro25 <aaditya2002pramod@gmail.com>
2021-12-18 18:44:34 +0530

    added already completed functions

--- Task 3/bm_task_3/task_3.py
+++ Task 3/bm_task_3/task_3.py
@@ -16,14 +16,12 @@
 *****************************************************************************************
 '''
 
-
 # Team ID:			[ Team-ID ]
 # Author List:		[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
 # Filename:			task_3.py
-# Functions:		
-# Global variables:	
+# Functions:
+# Global variables:
 # 					[ List of global variables defined in this file ]
-
 
 ####################### IMPORT MODULES #######################
 ## You are not allowed to make any changes in this section. ##
@@ -42,18 +40,17 @@ from pyzbar.pyzbar import decode
 
 ##############################################################
 
-
 # Importing the sim module for Remote API connection with CoppeliaSim
 try:
-	import sim
-	
+    import sim
+
 except Exception:
-	print('\n[ERROR] It seems the sim.py OR simConst.py files are not found!')
-	print('\n[WARNING] Make sure to have following files in the directory:')
-	print('sim.py, simConst.py and appropriate library - remoteApi.dll (if on Windows), remoteApi.so (if on Linux) or remoteApi.dylib (if on Mac).\n')
-	sys.exit()
-
-
+    print('\n[ERROR] It seems the sim.py OR simConst.py files are not found!')
+    print('\n[WARNING] Make sure to have following files in the directory:')
+    print(
+        'sim.py, simConst.py and appropriate library - remoteApi.dll (if on Windows), remoteApi.so (if on Linux) or remoteApi.dylib (if on Mac).\n'
+    )
+    sys.exit()
 
 ################# ADD UTILITY FUNCTIONS HERE #################
 ## You can define any utility functions for your code.      ##
@@ -61,17 +58,11 @@ except Exception:
 ## readable and easy to understand.                         ##
 ##############################################################
 
-
-
-
-
-
 ##############################################################
 
 
 def init_remote_api_server():
-
-	"""
+    """
 	Purpose:
 	---
 	This function should first close any open connections and then start
@@ -92,20 +83,19 @@ def init_remote_api_server():
 	
 	"""
 
-	client_id = -1
+    client_id = -1
 
-	##############	ADD YOUR CODE HERE	##############
+    ##############	ADD YOUR CODE HERE	##############
+    sim.simxFinish(-1)
+    client_id = sim.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
 
+    ##################################################
 
-
-	##################################################
-
-	return client_id
+    return client_id
 
 
 def start_simulation(client_id):
-
-	"""
+    """
 	Purpose:
 	---
 	This function should first start the simulation if the connection to server
@@ -127,19 +117,18 @@ def start_simulation(client_id):
 	return_code = start_simulation()
 	
 	"""
-	return_code = -2
+    return_code = -2
 
-	##############	ADD YOUR CODE HERE	##############
+    ##############	ADD YOUR CODE HERE	##############
+    return_code = sim.simxStartSimulation(client_id, sim.simx_opmode_oneshot)
 
+    ##################################################
 
-	##################################################
-
-	return return_code
+    return return_code
 
 
 def get_vision_sensor_image(client_id):
-	
-	"""
+    """
 	Purpose:
 	---
 	This function should first get the handle of the Vision Sensor object from the scene.
@@ -163,20 +152,21 @@ def get_vision_sensor_image(client_id):
 	vision_sensor_image, image_resolution, return_code = get_vision_sensor_image()
 	"""
 
+    return_code = 0
 
-	return_code = 0
+    ##############	ADD YOUR CODE HERE	##############
+    res, v0 = sim.simxGetObjectHandle(client_id, 'vision_sensor',
+                                      sim.simx_opmode_oneshot_wait)
+    return_code, image_resolution, vision_sensor_image = sim.simxGetVisionSensorImage(
+        client_id, v0, 0, sim.simx_opmode_oneshot_wait)
 
-	##############	ADD YOUR CODE HERE	##############
+    ##################################################
 
-
-	##################################################
-
-	return vision_sensor_image, image_resolution, return_code
+    return vision_sensor_image, image_resolution, return_code
 
 
 def transform_vision_sensor_image(vision_sensor_image, image_resolution):
-
-	"""
+    """
 	Purpose:
 	---
 	This function should:
@@ -205,19 +195,24 @@ def transform_vision_sensor_image(vision_sensor_image, image_resolution):
 	
 	"""
 
-	transformed_image = None
+    transformed_image = None
 
-	##############	ADD YOUR CODE HERE	##############
+    ##############	ADD YOUR CODE HERE	##############
+    image = np.array(np.uint8(vision_sensor_image))
+    #print(image.reshape((image_resolution[1],-1)).shape)
+    #print(min(image))
+    transformed_image = np.reshape(
+        image, [image_resolution[0], image_resolution[1], 3])
+    transformed_image = cv2.flip(transformed_image, 1)
+    transformed_image = cv2.cvtColor(transformed_image, cv2.COLOR_BGR2RGB)
 
+    ##################################################
 
-
-	##################################################
-	
-	return transformed_image
+    return transformed_image
 
 
 def stop_simulation(client_id):
-	"""
+    """
 	Purpose:
 	---
 	This function should stop the running simulation in CoppeliaSim server.
@@ -240,19 +235,18 @@ def stop_simulation(client_id):
 	
 	"""
 
-	return_code = -2
+    return_code = -2
 
-	##############	ADD YOUR CODE HERE	##############
-	
+    ##############	ADD YOUR CODE HERE	##############
+    return_code = sim.simxStopSimulation(client_id, sim.simx_opmode_oneshot)
 
-	##################################################
+    ##################################################
 
-	return return_code
+    return return_code
 
 
 def exit_remote_api_server(client_id):
-	
-	"""
+    """
 	Purpose:
 	---
 	This function should wait for the last command sent to arrive at the Coppeliasim server
@@ -273,15 +267,14 @@ def exit_remote_api_server(client_id):
 	
 	"""
 
-	##############	ADD YOUR CODE HERE	##############
-	
+    ##############	ADD YOUR CODE HERE	##############
+    sim.simxFinish(client_id)
 
-	##################################################
+    ##################################################
 
 
 def detect_qr_codes(transformed_image):
-	
-	"""
+    """
 	Purpose:
 	---
 	This function receives the transformed image from the vision sensor and detects qr codes in the image
@@ -301,17 +294,22 @@ def detect_qr_codes(transformed_image):
 	
 	"""
 
-	##############	ADD YOUR CODE HERE	##############
-	
+    ##############	ADD YOUR CODE HERE	##############
+    qr_codes = []
+    barcodes = decode(transformed_image)
+    for bar in barcodes:
+        (x, y, w, h) = bar.rect
+        data = bar.data.decode("utf-8")
+        qr_codes.append([data, (x + w / 2, y + h / 2)])
 
-	##################################################
-	
-	return qr_codes
+    ##################################################
+
+    return qr_codes
 
 
-def set_bot_movement(client_id,wheel_joints,forw_back_vel,left_right_vel,rot_vel):
-
-	"""
+def set_bot_movement(client_id, wheel_joints, forw_back_vel, left_right_vel,
+                     rot_vel):
+    """
 	Purpose:
 	---
 	This function takes desired forward/back, left/right, rotational velocites of the bot as input arguments.
@@ -345,15 +343,13 @@ def set_bot_movement(client_id,wheel_joints,forw_back_vel,left_right_vel,rot_vel
 	
 	"""
 
-	##############	ADD YOUR CODE HERE	##############
-	
+    ##############	ADD YOUR CODE HERE	##############
 
-	##################################################
+    ##################################################
 
 
 def init_setup(client_id):
-	
-	"""
+    """
 	Purpose:
 	---
 	This function will get the object handles of all the four joints in the bot, store them in a list
@@ -375,17 +371,21 @@ def init_setup(client_id):
 	
 	"""
 
-	##############	ADD YOUR CODE HERE	##############
-	
+    ##############	ADD YOUR CODE HERE	##############
+    wheel_joints = []
+    wheels = ['fr', 'fl', 'rr', 'rl']
+    wheel_joints = [
+        sim.simxGetObjectHandle(client_id, f'rollingJoint_{i}',
+                                sim.simx_opmode_oneshot_wait)[1]
+        for i in wheels
+    ]
+    ##################################################
 
-	##################################################
-
-	return wheel_joints
+    return wheel_joints
 
 
 def encoders(client_id):
-
-	"""
+    """
 	Purpose:
 	---
 	This function will get the `combined_joint_position` string signal from CoppeliaSim, decode it
@@ -407,18 +407,19 @@ def encoders(client_id):
 	
 	"""
 
-	return_code,signal_value=sim.simxGetStringSignal(client_id,'combined_joint_position',sim.simx_opmode_blocking)
-	signal_value = signal_value.decode()
-	joints_position = signal_value.split("%")
+    return_code, signal_value = sim.simxGetStringSignal(
+        client_id, 'combined_joint_position', sim.simx_opmode_blocking)
+    signal_value = signal_value.decode()
+    joints_position = signal_value.split("%")
 
-	for index,joint_val in enumerate(joints_position):
-		joints_position[index]=float(joint_val)
+    for index, joint_val in enumerate(joints_position):
+        joints_position[index] = float(joint_val)
 
-	return joints_position
+    return joints_position
 
 
 def nav_logic():
-	"""
+    """
 	Purpose:
 	---
 	This function should implement your navigation logic. 
@@ -426,7 +427,7 @@ def nav_logic():
 
 
 def shortest_path():
-	"""
+    """
 	Purpose:
 	---
 	This function should be used to find the shortest path on the given floor between the destination and source co-ordinates.
@@ -434,13 +435,12 @@ def shortest_path():
 
 
 def task_3_primary(client_id, target_points):
-	
-	"""
+    """
 	Purpose:
 	---
 	
 	# NOTE:This is the only function that is called from the main function and from the executable.
-	
+
 	Make sure to call all the necessary functions (apart from the ones called in the main) according to your logic. 
 	The bot should traverses all the target navigational co-ordinates.
 
@@ -460,110 +460,144 @@ def task_3_primary(client_id, target_points):
 	target_points(client_id, target_points)
 	
 	"""
-
+    init_setup(client_id)
 
 
 if __name__ == "__main__":
 
-	##################################################
-	# target_points is a list of tuples. These tuples are the target navigational co-ordinates
-	# target_points = [(x1,y1),(x2,y2),(x3,y3),(x4,y4)...]
-	# example:
-	target_points = [(2,3),(3,6),(11,11),(0,0)]    # You can give any number of different co-ordinates
+    ##################################################
+    # target_points is a list of tuples. These tuples are the target navigational co-ordinates
+    # target_points = [(x1,y1),(x2,y2),(x3,y3),(x4,y4)...]
+    # example:
+    target_points = [(2, 3), (3, 6), (11, 11), (0, 0)
+                     ]  # You can give any number of different co-ordinates
 
+    ##################################################
+    ## NOTE: You are NOT allowed to make any changes in the code below ##
 
-	##################################################
-	## NOTE: You are NOT allowed to make any changes in the code below ##
+    # Initiate the Remote API connection with CoppeliaSim server
+    print('\nConnection to CoppeliaSim Remote API Server initiated.')
+    print('Trying to connect to Remote API Server...')
 
-	# Initiate the Remote API connection with CoppeliaSim server
-	print('\nConnection to CoppeliaSim Remote API Server initiated.')
-	print('Trying to connect to Remote API Server...')
+    try:
+        client_id = init_remote_api_server()
+        if (client_id != -1):
+            print(
+                '\nConnected successfully to Remote API Server in CoppeliaSim!'
+            )
 
-	try:
-		client_id = init_remote_api_server()
-		if (client_id != -1):
-			print('\nConnected successfully to Remote API Server in CoppeliaSim!')
+            # Starting the Simulation
+            try:
+                return_code = start_simulation(client_id)
 
-			# Starting the Simulation
-			try:
-				return_code = start_simulation(client_id)
+                if (return_code == sim.simx_return_novalue_flag) or (
+                        return_code == sim.simx_return_ok):
+                    print('\nSimulation started correctly in CoppeliaSim.')
 
-				if (return_code == sim.simx_return_novalue_flag) or (return_code == sim.simx_return_ok):
-					print('\nSimulation started correctly in CoppeliaSim.')
+                else:
+                    print(
+                        '\n[ERROR] Failed starting the simulation in CoppeliaSim!'
+                    )
+                    print(
+                        'start_simulation function is not configured correctly, check the code!'
+                    )
+                    print()
+                    sys.exit()
 
-				else:
-					print('\n[ERROR] Failed starting the simulation in CoppeliaSim!')
-					print('start_simulation function is not configured correctly, check the code!')
-					print()
-					sys.exit()
+            except Exception:
+                print(
+                    '\n[ERROR] Your start_simulation function throwed an Exception, kindly debug your code!'
+                )
+                print('Stop the CoppeliaSim simulation manually.\n')
+                traceback.print_exc(file=sys.stdout)
+                print()
+                sys.exit()
 
-			except Exception:
-				print('\n[ERROR] Your start_simulation function throwed an Exception, kindly debug your code!')
-				print('Stop the CoppeliaSim simulation manually.\n')
-				traceback.print_exc(file=sys.stdout)
-				print()
-				sys.exit()
+        else:
+            print('\n[ERROR] Failed connecting to Remote API server!')
+            print(
+                '[WARNING] Make sure the CoppeliaSim software is running and')
+            print(
+                '[WARNING] Make sure the Port number for Remote API Server is set to 19997.'
+            )
+            print(
+                '[ERROR] OR init_remote_api_server function is not configured correctly, check the code!'
+            )
+            print()
+            sys.exit()
 
-		else:
-			print('\n[ERROR] Failed connecting to Remote API server!')
-			print('[WARNING] Make sure the CoppeliaSim software is running and')
-			print('[WARNING] Make sure the Port number for Remote API Server is set to 19997.')
-			print('[ERROR] OR init_remote_api_server function is not configured correctly, check the code!')
-			print()
-			sys.exit()
+    except Exception:
+        print(
+            '\n[ERROR] Your init_remote_api_server function throwed an Exception, kindly debug your code!'
+        )
+        print('Stop the CoppeliaSim simulation manually if started.\n')
+        traceback.print_exc(file=sys.stdout)
+        print()
+        sys.exit()
 
-	except Exception:
-		print('\n[ERROR] Your init_remote_api_server function throwed an Exception, kindly debug your code!')
-		print('Stop the CoppeliaSim simulation manually if started.\n')
-		traceback.print_exc(file=sys.stdout)
-		print()
-		sys.exit()
+    try:
 
-	try:
+        task_3_primary(client_id, target_points)
+        time.sleep(1)
 
-		task_3_primary(client_id, target_points)
-		time.sleep(1)        
+        try:
+            return_code = stop_simulation(client_id)
+            if (return_code == sim.simx_return_ok) or (
+                    return_code == sim.simx_return_novalue_flag):
+                print('\nSimulation stopped correctly.')
 
-		try:
-			return_code = stop_simulation(client_id)                            
-			if (return_code == sim.simx_return_ok) or (return_code == sim.simx_return_novalue_flag):
-				print('\nSimulation stopped correctly.')
+                # Stop the Remote API connection with CoppeliaSim server
+                try:
+                    exit_remote_api_server(client_id)
+                    if (start_simulation(client_id) ==
+                            sim.simx_return_initialize_error_flag):
+                        print(
+                            '\nDisconnected successfully from Remote API Server in CoppeliaSim!'
+                        )
 
-				# Stop the Remote API connection with CoppeliaSim server
-				try:
-					exit_remote_api_server(client_id)
-					if (start_simulation(client_id) == sim.simx_return_initialize_error_flag):
-						print('\nDisconnected successfully from Remote API Server in CoppeliaSim!')
+                    else:
+                        print(
+                            '\n[ERROR] Failed disconnecting from Remote API server!'
+                        )
+                        print(
+                            '[ERROR] exit_remote_api_server function is not configured correctly, check the code!'
+                        )
 
-					else:
-						print('\n[ERROR] Failed disconnecting from Remote API server!')
-						print('[ERROR] exit_remote_api_server function is not configured correctly, check the code!')
+                except Exception:
+                    print(
+                        '\n[ERROR] Your exit_remote_api_server function throwed an Exception, kindly debug your code!'
+                    )
+                    print('Stop the CoppeliaSim simulation manually.\n')
+                    traceback.print_exc(file=sys.stdout)
+                    print()
+                    sys.exit()
 
-				except Exception:
-					print('\n[ERROR] Your exit_remote_api_server function throwed an Exception, kindly debug your code!')
-					print('Stop the CoppeliaSim simulation manually.\n')
-					traceback.print_exc(file=sys.stdout)
-					print()
-					sys.exit()
-									  
-			else:
-				print('\n[ERROR] Failed stopping the simulation in CoppeliaSim server!')
-				print('[ERROR] stop_simulation function is not configured correctly, check the code!')
-				print('Stop the CoppeliaSim simulation manually.')
-		  
-			print()
-			sys.exit()
+            else:
+                print(
+                    '\n[ERROR] Failed stopping the simulation in CoppeliaSim server!'
+                )
+                print(
+                    '[ERROR] stop_simulation function is not configured correctly, check the code!'
+                )
+                print('Stop the CoppeliaSim simulation manually.')
 
-		except Exception:
-			print('\n[ERROR] Your stop_simulation function throwed an Exception, kindly debug your code!')
-			print('Stop the CoppeliaSim simulation manually.\n')
-			traceback.print_exc(file=sys.stdout)
-			print()
-			sys.exit()
+            print()
+            sys.exit()
 
-	except Exception:
-		print('\n[ERROR] Your task_3_primary function throwed an Exception, kindly debug your code!')
-		print('Stop the CoppeliaSim simulation manually if started.\n')
-		traceback.print_exc(file=sys.stdout)
-		print()
-		sys.exit()
+        except Exception:
+            print(
+                '\n[ERROR] Your stop_simulation function throwed an Exception, kindly debug your code!'
+            )
+            print('Stop the CoppeliaSim simulation manually.\n')
+            traceback.print_exc(file=sys.stdout)
+            print()
+            sys.exit()
+
+    except Exception:
+        print(
+            '\n[ERROR] Your task_3_primary function throwed an Exception, kindly debug your code!'
+        )
+        print('Stop the CoppeliaSim simulation manually if started.\n')
+        traceback.print_exc(file=sys.stdout)
+        print()
+        sys.exit()
