5ce0cf95db6daa572208832cd029d5d1c54f866b
madpro25 <aaditya2002pramod@gmail.com>
2022-01-11 14:49:26 +0530

    completed task 3

--- Task 3/bm_task_3/task_3.py
+++ Task 3/bm_task_3/task_3.py
@@ -62,7 +62,7 @@ except Exception:
 
 
 def init_remote_api_server():
-    """
+	"""
 	Purpose:
 	---
 	This function should first close any open connections and then start
@@ -82,16 +82,15 @@ def init_remote_api_server():
 	client_id = init_remote_api_server()
 	
 	"""
-
-    client_id = -1
+	global client_id
+	client_id = -1
 
     ##############	ADD YOUR CODE HERE	##############
-    sim.simxFinish(-1)
-    client_id = sim.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
+	sim.simxFinish(-1)
+	client_id = sim.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
 
     ##################################################
-
-    return client_id
+	return client_id
 
 
 def start_simulation(client_id):
@@ -155,7 +154,7 @@ def get_vision_sensor_image(client_id):
     return_code = 0
 
     ##############	ADD YOUR CODE HERE	##############
-    res, v0 = sim.simxGetObjectHandle(client_id, 'vision_sensor',
+    res, v0 = sim.simxGetObjectHandle(client_id, 'vision_sensor_1',
                                       sim.simx_opmode_oneshot_wait)
     return_code, image_resolution, vision_sensor_image = sim.simxGetVisionSensorImage(
         client_id, v0, 0, sim.simx_opmode_oneshot_wait)
@@ -166,7 +165,7 @@ def get_vision_sensor_image(client_id):
 
 
 def transform_vision_sensor_image(vision_sensor_image, image_resolution):
-    """
+	"""
 	Purpose:
 	---
 	This function should:
@@ -194,21 +193,19 @@ def transform_vision_sensor_image(vision_sensor_image, image_resolution):
 	transformed_image = transform_vision_sensor_image(vision_sensor_image, image_resolution)
 	
 	"""
-
-    transformed_image = None
+	transformed_image = None
 
     ##############	ADD YOUR CODE HERE	##############
-    image = np.array(np.uint8(vision_sensor_image))
+	image = np.array(np.uint8(vision_sensor_image))
     #print(image.reshape((image_resolution[1],-1)).shape)
     #print(min(image))
-    transformed_image = np.reshape(
+	transformed_image = np.reshape(
         image, [image_resolution[0], image_resolution[1], 3])
-    transformed_image = cv2.flip(transformed_image, 1)
-    transformed_image = cv2.cvtColor(transformed_image, cv2.COLOR_BGR2RGB)
+	transformed_image = cv2.flip(transformed_image, 0)
+	transformed_image = cv2.cvtColor(transformed_image, cv2.COLOR_BGR2GRAY)
 
     ##################################################
-
-    return transformed_image
+	return transformed_image
 
 
 def stop_simulation(client_id):
@@ -273,8 +270,8 @@ def exit_remote_api_server(client_id):
     ##################################################
 
 
-def detect_qr_codes(transformed_image):
-    """
+def detect_qr_codes(transformed_image,client_id):
+	"""
 	Purpose:
 	---
 	This function receives the transformed image from the vision sensor and detects qr codes in the image
@@ -295,21 +292,35 @@ def detect_qr_codes(transformed_image):
 	"""
 
     ##############	ADD YOUR CODE HERE	##############
-    qr_codes = []
-    barcodes = decode(transformed_image)
-    for bar in barcodes:
-        (x, y, w, h) = bar.rect
-        data = bar.data.decode("utf-8")
-        qr_codes.append([data, (x + w / 2, y + h / 2)])
+	global wheel_joints
+	point = []
+	barcodes = decode(transformed_image)
+	for bar in barcodes:
+		(x, y, w, h) = bar.rect
+		data = bar.data.decode("utf-8")
+		data=data[1:-1].split(",")
+		point=[int(i) for i in data]
+		if x<10:
+			set_bot_movement(client_id,wheel_joints,0,-2,0)
+		elif x+w>110:
+			set_bot_movement(client_id,wheel_joints,0,2,0)
+		elif y<10:
+			set_bot_movement(client_id,wheel_joints,1,0,0)
+		elif y+h>110:
+			set_bot_movement(client_id,wheel_joints,-1,0,0)
+
+		
+
+		
+		#qr_codes.append(data)
 
     ##################################################
-
-    return qr_codes
+	return point
 
 
 def set_bot_movement(client_id, wheel_joints, forw_back_vel, left_right_vel,
                      rot_vel):
-    """
+	"""
 	Purpose:
 	---
 	This function takes desired forward/back, left/right, rotational velocites of the bot as input arguments.
@@ -340,16 +351,52 @@ def set_bot_movement(client_id, wheel_joints, forw_back_vel, left_right_vel,
 	Example call:
 	---
 	set_bot_movement(client_id, wheel_joints, 0.5, 0, 0)
-	
 	"""
 
     ##############	ADD YOUR CODE HERE	##############
+	vel=[0,0,0,0]
+	#FORWARD BACKWARD MOTION
+	# sim.simxSetJointTargetVelocity(client_id,wheel_joints[0],forw_back_vel,sim.simx_opmode_oneshot)
+	# sim.simxSetJointTargetVelocity(client_id,wheel_joints[1],forw_back_vel,sim.simx_opmode_oneshot)
+	# sim.simxSetJointTargetVelocity(client_id,wheel_joints[2],forw_back_vel,sim.simx_opmode_oneshot)
+	# sim.simxSetJointTargetVelocity(client_id,wheel_joints[3],forw_back_vel,sim.simx_opmode_oneshot)
+	vel[0]+=forw_back_vel
+	vel[1]+=forw_back_vel
+	vel[2]+=forw_back_vel
+	vel[3]+=forw_back_vel
+
+	#LEFT RIGHT MOTION
+	# sim.simxSetJointTargetVelocity(client_id,wheel_joints[0],left_right_vel*math.sqrt(2),sim.simx_opmode_oneshot)
+	# sim.simxSetJointTargetVelocity(client_id,wheel_joints[1],-left_right_vel*math.sqrt(2),sim.simx_opmode_oneshot)
+	# sim.simxSetJointTargetVelocity(client_id,wheel_joints[2],-left_right_vel*math.sqrt(2),sim.simx_opmode_oneshot)
+	# sim.simxSetJointTargetVelocity(client_id,wheel_joints[3],left_right_vel*math.sqrt(2),sim.simx_opmode_oneshot)
+	vel[0]+=-left_right_vel
+	vel[1]+=left_right_vel
+	vel[2]+=left_right_vel
+	vel[3]+=-left_right_vel
+
+	#ROTATION
+	# sim.simxSetJointTargetVelocity(client_id,wheel_joints[0],rot_vel,sim.simx_opmode_oneshot)
+	# sim.simxSetJointTargetVelocity(client_id,wheel_joints[1],-rot_vel,sim.simx_opmode_oneshot)
+	# sim.simxSetJointTargetVelocity(client_id,wheel_joints[2],rot_vel,sim.simx_opmode_oneshot)
+	# sim.simxSetJointTargetVelocity(client_id,wheel_joints[3],-rot_vel,sim.simx_opmode_oneshot)
+	vel[0]+=-rot_vel
+	vel[1]+=rot_vel
+	vel[2]+=-rot_vel
+	vel[3]+=rot_vel
+
+	sim.simxSetJointTargetVelocity(client_id,wheel_joints[0],vel[0],sim.simx_opmode_streaming)
+	sim.simxSetJointTargetVelocity(client_id,wheel_joints[1],vel[1],sim.simx_opmode_streaming)
+	sim.simxSetJointTargetVelocity(client_id,wheel_joints[2],vel[2],sim.simx_opmode_streaming)
+	sim.simxSetJointTargetVelocity(client_id,wheel_joints[3],vel[3],sim.simx_opmode_streaming)
+
+
 
     ##################################################
 
 
 def init_setup(client_id):
-    """
+	"""
 	Purpose:
 	---
 	This function will get the object handles of all the four joints in the bot, store them in a list
@@ -372,16 +419,16 @@ def init_setup(client_id):
 	"""
 
     ##############	ADD YOUR CODE HERE	##############
-    wheel_joints = []
-    wheels = ['fr', 'fl', 'rr', 'rl']
-    wheel_joints = [
+	global wheel_joints
+	wheel_joints=[]
+	wheels = ['fr', 'fl', 'rr', 'rl']
+	wheel_joints = [
         sim.simxGetObjectHandle(client_id, f'rollingJoint_{i}',
                                 sim.simx_opmode_oneshot_wait)[1]
         for i in wheels
     ]
     ##################################################
-
-    return wheel_joints
+	return wheel_joints
 
 
 def encoders(client_id):
@@ -418,13 +465,29 @@ def encoders(client_id):
     return joints_position
 
 
-def nav_logic():
-    """
+def nav_logic(curr_coord,next_coord,vel):
+	"""
 	Purpose:
 	---
 	This function should implement your navigation logic. 
 	"""
-
+	#------------------------------
+	# GRID MOVEMENTS
+	#------------------------------
+	x,y=curr_coord
+	X,Y=next_coord
+	forw_vel,left_right_vel,rot_vel=0,0,0
+	if x<X:
+		left_right_vel=vel
+	elif x>X:
+		left_right_vel=-vel
+	elif x==X:
+		if y<Y:
+			forw_vel=vel
+		elif y>Y:
+			forw_vel=-vel
+	
+	return forw_vel,left_right_vel,rot_vel
 
 def shortest_path():
     """
@@ -435,7 +498,7 @@ def shortest_path():
 
 
 def task_3_primary(client_id, target_points):
-    """
+	"""
 	Purpose:
 	---
 	
@@ -458,9 +521,56 @@ def task_3_primary(client_id, target_points):
 	Example call:
 	---
 	target_points(client_id, target_points)
-	
 	"""
-    init_setup(client_id)
+	# NOTE:max achievable speed without errors is 7
+	global x
+	x=[0,0]
+	wheel_joints=init_setup(client_id)
+	img,res,ret=get_vision_sensor_image(client_id)
+	img=transform_vision_sensor_image(img,res)
+	temp=detect_qr_codes(img,client_id)
+	if len(temp)==2:
+		x=temp
+
+	#print(x)
+	for target_point in target_points:
+		while x != list(target_point):
+			forw_vel,left_right_vel,rot_vel=nav_logic(x,target_point,7)
+			set_bot_movement(client_id,wheel_joints,forw_vel,left_right_vel,rot_vel)
+			img,res,ret=get_vision_sensor_image(client_id)
+			img=transform_vision_sensor_image(img,res)
+			temp=detect_qr_codes(img,client_id)
+			if len(temp)==2:
+				x=temp
+
+			#print(x,target_point)
+	
+	set_bot_movement(client_id,wheel_joints,0,0,0)
+
+	
+	# i=0
+	# while(i!='x'):
+	# 	i=input()
+	# 	if i == 'left':
+	# 		set_bot_movement(client_id,wheel_joints,0,-5,0)
+	# 	elif i == 'right':
+	# 		set_bot_movement(client_id,wheel_joints,0,5,0)
+	# 	elif i == 'forw':
+	# 		set_bot_movement(client_id,wheel_joints,5,0,0)
+	# 	elif i == 'back':
+	# 		set_bot_movement(client_id,wheel_joints,-5,0,0)
+	# 	elif i == 'rotr':
+	# 		set_bot_movement(client_id,wheel_joints,0,0,5)
+	# 	elif i == 'rotl':
+	# 		set_bot_movement(client_id,wheel_joints,0,0,-5)
+	# 	else: 
+	# 		set_bot_movement(client_id,wheel_joints,0,0,0)
+
+	# 	img,res,ret=get_vision_sensor_image(client_id)
+	# 	img=transform_vision_sensor_image(img,res)
+	# 	x=detect_qr_codes(img)
+	# 	print(x)
+		
 
 
 if __name__ == "__main__":
@@ -469,7 +579,7 @@ if __name__ == "__main__":
     # target_points is a list of tuples. These tuples are the target navigational co-ordinates
     # target_points = [(x1,y1),(x2,y2),(x3,y3),(x4,y4)...]
     # example:
-    target_points = [(2, 3), (3, 6), (11, 11), (0, 0)
+    target_points = [(2, 3), (3, 6)
                      ]  # You can give any number of different co-ordinates
 
     ##################################################
